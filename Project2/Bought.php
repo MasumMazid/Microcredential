1d3cdd653b7d5ec6a2452b770b09891abab8f996
MasumMazid <102322868+MasumMazid@users.noreply.github.com>
2022-08-09 20:43:42 -0400

    Add files via upload

---
+++ Project2/Bought.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <b>THANK YOU FOR YOUR PURCHASE</b>
+    
+  </body>
+</html>
---
+++ Project2/project_2.js
@@ -0,0 +1,20 @@
+var boats=[
+{name:"Oro Jackson ",price:240000,color:"red",speed:200},
+{name:"Thousand Sunny",price:500000,color:"red",speed:250}
+{name:"Lomac 520",price:23200,color:"red",speed:86}
+{name:"Model T rider",price:67300,color:"blue",speed:43}
+{name:"Yachmaster Cruiser -T 2020",price:5234500,color:"blue",speed:150}
+{name:"Santa Explorer",price:50000,color:"blue",speed:123}
+{name:"Kayak",price:150,color:"green",speed:10}
+{name:"Yamamoto",price:83838,color:"green",speed:166}
+{name:"Elric",price:45230,color:"green",speed:114}
+]
+
+
+function getprice(){
+let x=prompt('Give me a price')
+for(let y=0;y<boats.length();y++){
+let p=boat[0].price
+if (boat[].price<x){
+
+}
